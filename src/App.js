fba65fdf436645db207072520adc5a88025015d3
CAM603 <cameronhawley@comcast.net>
2020-05-28 11:19:57 -0400

    add scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState, useCallback, useRef } from "react";
-import "./App.css";
+import React from "react";
 import Game from "./components/Game";
+import "./App.scss";
 
 function App() {
-    return <Game />;
+    return (
+        <div className="App">
+            <Game />
+        </div>
+    );
 }
 
 export default App;
